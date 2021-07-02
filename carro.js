10df7153a6b81d5cf7715dbe8e43f88c2b29fcd2
Rafael <rafaelswitek@gmail.com>
2021-07-01 23:23:45 -0300

    Aula 03

--- carro.js
+++ carro.js
@@ -1,40 +1,29 @@
 // Códigos do carro
-// Carro 1
-let xCarro = 600
-let yCarro = 40
-let velocidadeCarro = 2
 
-// Carro 2
-let xCarro2 = 600
-let yCarro2 = 96
-let velocidadeCarro2 = 2.5
-
-
-// Carro 3
-let xCarro3 = 600
-let yCarro3 = 150
-let velocidadeCarro3 = 3
+let xCarros = [600, 600, 600]
+let yCarros = [40, 96, 150]
+let velocidadeCarros = [2, 2.5, 3]
 
 function mostraCarro() {
-  image(imagemCarro, xCarro, yCarro, 50, 30)
-  image(imagemCarro2, xCarro2, yCarro2, 50, 30)
-  image(imagemCarro3, xCarro3, yCarro3, 50, 30)
+  for (let i = 0; i < imagemCarros.length; i++) {
+    image(imagemCarros[i], xCarros[i], yCarros[i], 50, 30)
+  }
 }
 
 function movimentaCarro() {
-  xCarro -= velocidadeCarro
-  xCarro2 -= velocidadeCarro2
-  xCarro3 -= velocidadeCarro3
+  for (let i = 0; i < imagemCarros.length; i++) {
+    xCarros[i] -= velocidadeCarros[i]
+  }
 }
 
 function voltaPosicaoInicial() {
-  if (xCarro < -50) {
-    xCarro = 600
+  for (let i = 0; i < imagemCarros.length; i++) {
+    if (carroPassouDaTela(xCarros[i])) {
+      xCarros[i] = 600
+    }
   }
-  if (xCarro2 < -50) {
-    xCarro2 = 600
-  }
-  if (xCarro3 < -50) {
-    xCarro3 = 600
-  }
+}
+
+function carroPassouDaTela(xCarro) {
+  return xCarro < -50
 }
